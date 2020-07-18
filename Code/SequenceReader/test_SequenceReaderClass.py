bc12b4e8601d370f9733df5383c354762d17c224
Tim Fornell <timfo734@student.liu.se>
2020-07-18 15:51:06 +0200

    Add test that was accidentally removed
    Change-Id: I26cb07339501c695afe516507d3737f97fa7705e

---
+++ Code/SequenceReader/test_SequenceReaderClass.py
@@ -0,0 +1,17 @@
+import pytest
+from pathlib import Path
+from SequenceReaderClass import *
+
+def test_that_sequence_frames_are_read_correctly():
+    sequence_path = Path.joinpath(Path.cwd(), "Code/SequenceReader/file_example_MP4_640_3MG.mp4")
+    seq_reader = SequenceReaderClass(sequence_path)
+
+    assert seq_reader.sequence_is_available() == True
+
+    for _ in range(0, 10):
+        assert seq_reader.get_new_image() is not None
+        assert seq_reader.get_current_image() is not None
+        assert seq_reader.image_is_available() == True
+
+    
+test_that_sequence_frames_are_read_correctly()
