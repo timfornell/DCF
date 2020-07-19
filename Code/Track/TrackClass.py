8736ff9a4c54a3decead67eab0caf561d04e6f3d
Tim Fornell <timfo734@student.liu.se>
2020-07-19 20:23:57 +0200

    Add empty functions definitions in TrackClass
    Change-Id: I1645a3cf60c23edda9b455e9925004807fcd837e

--- Code/Track/TrackClass.py
+++ Code/Track/TrackClass.py
@@ -7,3 +7,18 @@ class Track():
         self._end_point = end_point
         self._track_id = id
         self._cropped_image = None
+
+    def crop_image_to_track_ROI(self, image):
+        pass
+
+    def pre_process_cropped_image(self):
+        pass
+
+    def update_track_position(self):
+        pass
+
+    def update_filter(self):
+        pass
+
+    def train_initial_filter(self):
+        pass
