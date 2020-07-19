9acd13c16fb01459ffffc9dfaf566b9fe4b1fec3
Tim Fornell <timfo734@student.liu.se>
2020-07-19 17:46:12 +0200

    Make it possible to update track rois
    Change-Id: I19d4d88be36e6d75f89628e087d26b213bb6ba5b

--- Code/Track/TrackClass.py
+++ Code/Track/TrackClass.py
@@ -3,5 +3,6 @@ import numpy as np
 class Track():
     def __init__(self, roi):
         self._is_initialized = False
-        self._roi = roi
+        self._start_point = (0, 0)
+        self._end_point = (0, 0)
         self._cropped_image = None
