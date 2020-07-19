120facf2d26fae45a656f2404f7c5796e7aa74fc
Tim Fornell <timfo734@student.liu.se>
2020-07-19 16:48:52 +0200

    Added descriptions for GUI functions and small fix
    Change-Id: I7c8dbb0698d797cfa5cb060edf4125a6e9683338

--- Code/GUI/GuiClass.py
+++ Code/GUI/GuiClass.py
@@ -18,16 +18,28 @@ class GUI():
         pass
 
     def update_window(self, image):
+        """ This function takes an image that it will display and save a copy of. """
         self._current_image = image
         cv2.imshow(self._window_name, self._current_image)
 
     def update_rectangles(self):
+        """
+        The purpose of this function is to draw rectangles in the image.
+
+        The rectangles are either being drawn currently or has been drawn previously. If a rectangle is currently
+        being drawn it is placed at the back of the array self._rectangles_to_draw which also contains all previously
+        drawn rectangles. It is supposed to be called once per frame, therefore the overlay and output variables
+        that are needed are initialised here.
+        """
         self._overlay = self._current_image.copy()
         self._output  = self._current_image.copy()
         if self._rectangles_to_draw:
             self.draw_rectangles(self._rectangles_to_draw)
 
     def draw_rectangles(self, rectangles):
+        """
+        This function takes a list of rectangles to draw and draws it on the overlay that is saved internally.
+        """
         for rect in rectangles:
             cv2.rectangle(self._overlay, rect.start_point, rect.end_point, self._rectangle_color, self._rectangle_thickness)
 
@@ -41,6 +53,16 @@ class GUI():
         self._rectangles_to_draw = []
 
     def mouse_callback_function(self, event, x, y, flags, param):
+        """
+        The purpose of this function is to handle anything related to the mouse.
+
+        If the left mouse button is pressed it will initiate a drawing session by placing a rectangle at the last
+        position in the internal array '_rectangles_to_draw' with a corner located at the mouse pointer. While in a
+        drawing session and the mouse is moved around the size and width of this rectangle will be changed. When the
+        left mouse button is released the drawing session is ended and the position of the mouse pointer determines
+        where the second corner is placed. If the middle mouse button is clicked the last added rectangle will be
+        removed, if possible.
+        """
         if event == cv2.EVENT_LBUTTONDOWN:
             print("Left mouse was pressed")
             self._drawing = True
@@ -50,18 +72,14 @@ class GUI():
 
         elif event == cv2.EVENT_LBUTTONUP:
             print("Left mouse was released")
-
-            if self._drawing:
-                self._drawing = False
-                self._rectangles_to_draw.pop()
-
+            self._drawing = False
             self._drawing_end_position = (x, y)
             window = cv2.getWindowImageRect(self._window_name)
             top_left_x = max(min(self._drawing_start_position[0], self._drawing_end_position[0]), 0)
             top_left_y = max(min(self._drawing_start_position[1], self._drawing_end_position[1]), 0)
             bottom_right_x = min(max(self._drawing_start_position[0], self._drawing_end_position[0]), window[2] - 1)
             bottom_right_y = min(max(self._drawing_start_position[1], self._drawing_end_position[1]), window[3] - 1)
-            self._rectangles_to_draw.append(Rectangle((top_left_x, top_left_y), (bottom_right_x, bottom_right_y)))
+            self._rectangles_to_draw[-1] = Rectangle((top_left_x, top_left_y), (bottom_right_x, bottom_right_y))
 
         elif event == cv2.EVENT_MBUTTONDOWN:
             print("Middle click was pressed")
