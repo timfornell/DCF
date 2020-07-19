4721f80600b4f78b9f6367b6f54e5ccf7edc1572
Tim Fornell <timfo734@student.liu.se>
2020-07-19 20:09:46 +0200

    Forward drawn rectangles to tracker class
    Change-Id: I7a661fd38aad03c7f24028b78bece877515e175b

--- Code/Track/TrackClass.py
+++ Code/Track/TrackClass.py
@@ -1,8 +1,9 @@
 import numpy as np
 
 class Track():
-    def __init__(self, roi):
+    def __init__(self, start_point, end_point, id):
         self._is_initialized = False
-        self._start_point = (0, 0)
-        self._end_point = (0, 0)
+        self._start_point = start_point
+        self._end_point = end_point
+        self._track_id = id
         self._cropped_image = None
--- Code/Tracker/TrackerClass.py
+++ Code/Tracker/TrackerClass.py
@@ -4,27 +4,39 @@ from Track.TrackClass import Track
 
 class Tracker():
     def __init__(self):
-        self._tracks = np.empty(glob.MAX_NUMBER_OF_TRACKS, dtype=Track)
+        self._tracks = []
 
     def get_tracks(self):
         return self._tracks
 
     def initialize_tracks(self, list_of_track_rois):
-        # The ROIs from the GUI come as a list where each object is a dict containing 'width', 'height', 'top_left_x' and 'top_left_y'
+        """
+        The ROIs from the GUI come as a list where each object contains (top_left_x, top_left_y),
+        (bottom_right_x, bottom_right_y) and track id. This function is run each frame to catch any possible new
+        tracks that have been drawn.
+        """
+        internal_track_ids = [track._track_id for track in self._tracks]
+        incoming_track_ids = [track.id for track in list_of_track_rois]
         for track_roi in list_of_track_rois:
-            new_track = Track(track_roi)
-            np.append(self._tracks, new_track)
+            if track_roi.id not in internal_track_ids:
+                self._tracks.append(Track(track_roi.start_point, track_roi.end_point, track_roi.id))
+
+        for i, track in enumerate(self._tracks):
+            if track._track_id not in incoming_track_ids:
+                del self._tracks[i]
 
     def run_translation_tracker(self, current_image):
         """
-        This is the part of the DCF tracker where it looks for the object in the image using the filter that it has from the previous frame.
-        To do this a search window is used that is centered around the last known position of the track and the last known ROI increased by
-        X percent (this is a tunable parameter). The image is then preprocessed by applying a window (tunable). Then the filter is correlated
-        with the resulting image. If there is a clear enough peak in the resulting correlation the track is moved to this position. After that
-        a new cropped image is calculated, this image is used to update the filter so that it can be used to find the object in the next frame.
+        This is the part of the DCF tracker where it looks for the object in the image using the filter that it has
+        from the previous frame. To do this a search window is used that is centered around the last known position of
+        the track and the last known ROI increased by X percent (this is a tunable parameter). The image is then
+        preprocessed by applying a window (tunable). Then the filter is correlated with the resulting image. If there
+        is a clear enough peak in the resulting correlation the track is moved to this position. After that a new
+        cropped image is calculated, this image is used to update the filter so that it can be used to find the object
+        in the next frame.
 
-        The first frame a track exists is a bit special since the filter is trained. It is trained be perbutating the cropped image X (tunable)
-        amount of times.
+        The first frame a track exists is a bit special since the filter is trained. It is trained be perbutating the
+        cropped image X (tunable) amount of times.
         """
         for track in self.get_tracks():
             # Get the image patch from original image
